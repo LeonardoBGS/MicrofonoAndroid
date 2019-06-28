dfd85c353b84d8e1904eb1ac1014573e85dda899
Leonardo <uzergamermail@gmail.com>
2019-06-28 05:43:11 -0500

    se agregaron permisos y se logro verificar si hay microfono activo en api 21-23

--- MyApplication/app/src/main/java/com/mycompany/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/mycompany/com/myapplication/MainActivity.java
@@ -5,13 +5,16 @@
 package com.mycompany.com.myapplication;
 
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.media.AudioDeviceInfo;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
+import android.media.AudioRecordingConfiguration;
 import android.media.MediaRecorder;
 import android.media.MicrophoneInfo;
 import android.os.Build;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +23,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.content.Context;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -34,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
     private int BytesPerElement = 2; // 2 bytes in 16bit format
 
+    private int colorBtn = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 try {
+                    btn.setBackgroundColor( ( (colorBtn++)%2==0)? Color.GREEN: Color.YELLOW );
                     txtBox.setText(getMicrofonosActivos());
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -59,32 +66,66 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG,"getMicrofonosActivos");
         if(!hasMicrophone())
             return "No hay microfono";
-        Log.d(TAG,"Hay microfono");
         AudioRecord AR = new AudioRecord(MediaRecorder.AudioSource.MIC,
                 RECORDER_SAMPLERATE, RECORDER_CHANNELS,
                 RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            Log.d(TAG,"VER correcta");
-            List<MicrophoneInfo> activeMicrophones = AR.getActiveMicrophones();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            List<AudioRecordingConfiguration> listaMicrosActivos = audioManager.getActiveRecordingConfigurations ();
 
-            Log.d(TAG,"# microfonos = "+activeMicrophones.size());
-            return "# microfonos: "+activeMicrophones.size();
+            return "# microfonos activos: "+listaMicrosActivos.size();
         }
         else{
-            Log.d(TAG,"SDK Version muy baja");
+            Log.d(TAG,"API level < 24");
 
-            AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            MediaRecorder mRecorder;
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.HE_AAC);
+                mRecorder.setAudioEncodingBitRate(48000);
+            } else {
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+                mRecorder.setAudioEncodingBitRate(64000);
+            }
+            mRecorder.setAudioSamplingRate(16000);
+            File mOutputFile = getOutputFile();
+            mOutputFile.getParentFile().mkdirs();
+            mRecorder.setOutputFile(mOutputFile.getAbsolutePath());
 
-            //https://stackoverflow.com/questions/44949280/how-can-i-get-the-list-of-microphones-on-android-api-17-usb-connected-mics
-            //API 23 FUCK! 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                AudioDeviceInfo[] adi = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
-                Log.d(TAG,"LIST : "+adi.length);
+            boolean isMicroOcupado=false;
+
+            try {
+                mRecorder.prepare();
+                mRecorder.start();
+                Log.e("Stop recording","TRUE");
+                Log.d(TAG,"a parar");
+                try{
+                    mRecorder.stop();
+                }catch(RuntimeException stopException){
+                    Log.e("Voice Recorder", "stop() failed "+stopException.getMessage());
+                }
+                Log.d(TAG,"a release");
+                mRecorder.release();
+            } catch (IOException e) {
+
+                Log.e("Voice Recorder", "prepare() failed "+e.getMessage());
+            }catch (Exception e) {
+                isMicroOcupado=true;
+                Log.d(TAG,"error e:");
             }
 
-            return "Se detecto microfono - SDK Version muy baja";
+            mRecorder = null;
+            return ""+isMicroOcupado;
         }
+    }
+
+    private File getOutputFile() {
+        return new File(Environment.getExternalStorageDirectory().getAbsolutePath().toString()
+                + "/Voice Recorder/RECORDING_"
+                + ".m4a");
     }
 
     private boolean hasMicrophone() {
