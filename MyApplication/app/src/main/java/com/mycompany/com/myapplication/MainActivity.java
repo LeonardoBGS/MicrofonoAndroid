84591055c04c2ac154436fa0229857b91d27b96e
Lyo <leonardo.garcia@societao.com>
2019-06-28 18:14:49 -0500

    commit desde ubuntu

--- MyApplication/app/src/main/java/com/mycompany/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/mycompany/com/myapplication/MainActivity.java
@@ -123,4 +123,116 @@ public class MainActivity extends AppCompatActivity {
         return getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_MICROPHONE);
     }
+
+
+    /*
+    public boolean isMicroInUse() throws IOException {
+        if(!hasMicrophone())
+            Log.d(TAG,"no hay microfono");
+
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        Log.d(TAG, "micro mute?: "+audioManager.isMicrophoneMute());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            List<AudioRecordingConfiguration> listaMicrosActivos = audioManager.getActiveRecordingConfigurations ();
+
+            return (listaMicrosActivos.size()>0)?true:false;
+        }
+        else{
+            MediaRecorder mRecorder;
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.HE_AAC);
+                mRecorder.setAudioEncodingBitRate(48000);
+            } else {
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+                mRecorder.setAudioEncodingBitRate(64000);
+            }
+            mRecorder.setAudioSamplingRate(16000);
+            File mOutputFile = getOutputFile();
+            mOutputFile.getParentFile().mkdirs();
+            mRecorder.setOutputFile(mOutputFile.getAbsolutePath());
+
+            boolean isMicroOcupado=false;
+
+            try {
+                mRecorder.prepare();
+                mRecorder.start();
+                try{
+                    mRecorder.stop();
+                    mRecorder.release();
+                }catch(RuntimeException stopException){
+                    //android detecta que hay un stop despues de start
+                    //lanza exception en automatico
+                    //error: MediaRecorder: stop failed: -1007
+                }
+            } catch (IOException e) {
+            }catch (Exception e) {
+                //si el micro esta en uso lanza la exception
+                //error: MediaRecorder: start failed: -38
+                isMicroOcupado=true;
+            }
+            mRecorder = null;
+            return isMicroOcupado;
+        }
+    }
+
+    private File getOutputFile() {
+        return new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                + "/Voice Recorder/RECORDING_"
+                + ".m4a");
+    }
+
+    private boolean hasMicrophone() {
+        return getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_MICROPHONE);
+    }
+
+    private void iterateInstalledApps()
+    {
+        PackageManager p = this.getPackageManager();
+        final List <PackageInfo> appinstall =
+                p.getInstalledPackages(PackageManager.GET_PERMISSIONS);
+        for(PackageInfo pInfo:appinstall)
+        {
+            String[] reqPermission=pInfo.requestedPermissions;
+            if(reqPermission!=null)
+            {
+                for(int i=0;i<reqPermission.length;i++)
+                {
+                    if (((String)reqPermission[i]).equals("android.permission.RECORD_AUDIO"))
+                    {
+                        Log.d(TAG,pInfo.toString());
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void isMicroActive1(){
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am.getMode() == AudioManager.MODE_NORMAL){
+            Log.d(TAG, "normal mode");
+        }
+        else if(am.getMode() == AudioManager.MODE_IN_CALL){
+            Log.d(TAG, "call mode");
+        }
+        else if(am.getMode() == AudioManager.MODE_IN_COMMUNICATION){
+            Log.d(TAG, "voIP mode");
+        }
+        else
+            Log.d(TAG, "otro mode");
+    }
+
+
+    /*
+    MODE_IN_CALL solo funciona cuando se llama desde agente a viajero
+
+
+
+     */
 }
